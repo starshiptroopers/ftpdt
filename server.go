1cd2fe332ae6057e4f08808a1231159aaa06c689
mandalorian-one <mandalorian-one@users.noreply.github.com>
2021-02-23 13:10:36 +0500

    - some logging has been added

--- server.go
+++ server.go
@@ -90,5 +90,6 @@ func New(opts *Opts) (server *Ftpdt) {
 
 //ListenAndServe starts listening for ftp connection. It's blocking function
 func (ftpdt *Ftpdt) ListenAndServe() error {
+	ftpdt.Logger.Printf("", "Starting the ftp server at %s:%d", ftpdt.Hostname, ftpdt.Port)
 	return ftpdt.Server.ListenAndServe()
 }
